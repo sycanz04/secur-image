59e0f4ecda877e07de0ca2c701186b2e53bc03b0
sycanz04 <aidenchan0397@gmail.com>
2024-07-18 17:26:38 +0800

    feat: basic gui for main page

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from utils.account import loginAccount, createAccount, deleteAccount
 import cred
 import mysql.connector
 from mysql.connector import Error
+from tkinter import *
+import tkinter as tk
 
 
 try:
@@ -16,11 +18,11 @@ try:
         mycursor = conn.cursor()
         # Create database if not exist
         mycursor.execute(f"CREATE DATABASE IF NOT EXISTS {cred.database};")
-        print(f"Database '{cred.database}' created or already existed.")
+        # print(f"Database '{cred.database}' created or already existed.")
 
         # Use the newly created database
         mycursor.execute(f"USE {cred.database};")
-        print(f"Using database '{cred.database}'.")
+        # print(f"Using database '{cred.database}'.")
 
 except Error as e:
     print(f"Error: {e}")
@@ -38,36 +40,156 @@ mycursor.execute("""CREATE TABLE IF NOT EXISTS Images
                      userId INT NOT NULL,
                      FOREIGN KEY(userId) REFERENCES Users(userId));""")
 
+window = tk.Tk()
+window.title("Secur Image")
+frame1 = Frame(window)
+
+def prompt():
+    frame1.destroy()
+    frame2 = Frame(window)
+    frame2.pack()
+
+    usernameT = tk.Label(frame2, text="Username")
+    usernameT.grid(row=0)
+    
+    passwordT = tk.Label(frame2, text="Password")
+    passwordT.grid(row=1)
+
+    usernameTb = Entry(frame2)
+    usernameTb.grid(row=0, column=1)
+
+    passwdTb = Entry(frame2, show='*')
+    passwdTb.grid(row=1, column=1)
+
+    def handleLogin():
+        username = usernameTb.get()
+        passwd = passwdTb.get()
+        loginAccount(username, passwd, conn, mycursor)
+        
+
+    submitButton = tk.Button(frame2, 
+                        text="Login",
+                        command=handleLogin)
+    submitButton.grid(row=2, column=0, columnspan=2)
 
 def main():
-    print("""
-Account Options
-1. Login
-2. Signup
-3. Delete
-4. Quit
-""")
-    prompt = input("What do you want to do? ")
+    menuText = "Select an option"
+    menu = tk.Label(frame1, text=menuText)
+    menu.pack()
 
-    if prompt == "1":
-        username = input("Username: ")
-        passwd = input("Password: ").encode('utf-8')
-        loginAccount(username, passwd, conn, mycursor)
-    elif prompt == "2":
-        username = input("Username: ")
-        passwd = input("Password: ").encode('utf-8')
-        createAccount(username, passwd, conn, mycursor)
-    elif prompt == "3":
-        username = input("Username: ")
-        passwd = input("Password: ").encode('utf-8')
-        deleteAccount(username, passwd, conn, mycursor)
-    elif prompt == "4":
-        print("Goodbye!")
-        quit()
-    else:
-        print("\nInvalid option. Pick again!\n")
-        main()
+    button1 = tk.Button(frame1, 
+                        text="Login",
+                        command=prompt,
+                        activebackground="blue", 
+                        activeforeground="white",
+                        anchor="center",
+                        bd=3,
+                        bg="lightgray",
+                        cursor="hand2",
+                        disabledforeground="gray",
+                        fg="black",
+                        font=("Arial", 12),
+                        height=2,
+                        highlightbackground="black",
+                        highlightcolor="green",
+                        highlightthickness=2,
+                        justify="center",
+                        overrelief="raised",
+                        padx=10,
+                        pady=5,
+                        width=15,
+                        wraplength=100)
+    button1.pack()
+    button2 = tk.Button(frame1, 
+                        text="Signup", 
+                        activebackground="blue", 
+                        activeforeground="white",
+                        anchor="center",
+                        bd=3,
+                        bg="lightgray",
+                        cursor="hand2",
+                        disabledforeground="gray",
+                        fg="black",
+                        font=("Arial", 12),
+                        height=2,
+                        highlightbackground="black",
+                        highlightcolor="green",
+                        highlightthickness=2,
+                        justify="center",
+                        overrelief="raised",
+                        padx=10,
+                        pady=5,
+                        width=15,
+                        wraplength=100)
+    button2.pack()
+    button3 = tk.Button(frame1, 
+                        text="Delete", 
+                        activebackground="blue", 
+                        activeforeground="white",
+                        anchor="center",
+                        bd=3,
+                        bg="lightgray",
+                        cursor="hand2",
+                        disabledforeground="gray",
+                        fg="black",
+                        font=("Arial", 12),
+                        height=2,
+                        highlightbackground="black",
+                        highlightcolor="green",
+                        highlightthickness=2,
+                        justify="center",
+                        overrelief="raised",
+                        padx=10,
+                        pady=5,
+                        width=15,
+                        wraplength=100)
+    button3.pack()
+    button4 = tk.Button(frame1, 
+                        text="Quit", 
+                        command=window.destroy,
+                        activebackground="blue", 
+                        activeforeground="white",
+                        anchor="center",
+                        bd=3,
+                        bg="lightgray",
+                        cursor="hand2",
+                        disabledforeground="gray",
+                        fg="black",
+                        font=("Arial", 12),
+                        height=2,
+                        highlightbackground="black",
+                        highlightcolor="green",
+                        highlightthickness=2,
+                        justify="center",
+                        overrelief="raised",
+                        padx=10,
+                        pady=5,
+                        width=15,
+                        wraplength=100)
+    button4.pack()
 
+frame1.pack()
+main()
 
-if __name__ == "__main__":
-    main()
+window.mainloop()
+
+    # prompt = input("What do you want to do? ")
+
+    # if prompt == "1":
+    #     username = input("Username: ")
+    #     passwd = input("Password: ").encode('utf-8')
+    #     loginAccount(username, passwd, conn, mycursor)
+    # elif prompt == "2":
+    #     username = input("Username: ")
+    #     passwd = input("Password: ").encode('utf-8')
+    #     createAccount(username, passwd, conn, mycursor)
+    # elif prompt == "3":
+    #     username = input("Username: ")
+    #     passwd = input("Password: ").encode('utf-8')
+    #     deleteAccount(username, passwd, conn, mycursor)
+    # elif prompt == "4":
+    #     print("Goodbye!")
+    #     quit()
+    # else:
+    #     print("\nInvalid option. Pick again!\n")
+    #     main()
