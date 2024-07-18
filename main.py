8f4f75e3d2e0013133912e960f74c22e253126d3
sycanz04 <aidenchan0397@gmail.com>
2024-07-19 00:42:04 +0800

    feat: first wave GUI done

--- main.py
+++ main.py
@@ -1,76 +1,12 @@
 #! /usr/bin/env python3
-from utils.account import loginAccount, createAccount, deleteAccount
-import cred
-import mysql.connector
-from mysql.connector import Error
 from tkinter import *
 import tkinter as tk
-
-
-try:
-    conn = mysql.connector.connect(
-        host=cred.hostname,
-        user=cred.username,
-        passwd=cred.passwd,
-    )
-
-    if conn.is_connected():
-        mycursor = conn.cursor()
-        # Create database if not exist
-        mycursor.execute(f"CREATE DATABASE IF NOT EXISTS {cred.database};")
-        # print(f"Database '{cred.database}' created or already existed.")
-
-        # Use the newly created database
-        mycursor.execute(f"USE {cred.database};")
-        # print(f"Using database '{cred.database}'.")
-
-except Error as e:
-    print(f"Error: {e}")
-    quit()
-
-mycursor.execute("""CREATE TABLE IF NOT EXISTS Users
-                    (userId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                     username VARCHAR(50) NOT NULL,
-                     passwdHash VARCHAR(255) NOT NULL,
-                     secretKey VARCHAR(255) NOT NULL);""")
-mycursor.execute("""CREATE TABLE IF NOT EXISTS Images
-                    (photoId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                     platform VARCHAR(50) NOT NULL,
-                     photo LONGBLOB NOT NULL,
-                     userId INT NOT NULL,
-                     FOREIGN KEY(userId) REFERENCES Users(userId));""")
+from utils.mainMenu import login, create, delete
 
 window = tk.Tk()
 window.title("Secur Image")
+window.geometry('800x500')
 frame1 = Frame(window)
-
-def prompt():
-    frame1.destroy()
-    frame2 = Frame(window)
-    frame2.pack()
-
-    usernameT = tk.Label(frame2, text="Username")
-    usernameT.grid(row=0)
-    
-    passwordT = tk.Label(frame2, text="Password")
-    passwordT.grid(row=1)
-
-    usernameTb = Entry(frame2)
-    usernameTb.grid(row=0, column=1)
-
-    passwdTb = Entry(frame2, show='*')
-    passwdTb.grid(row=1, column=1)
-
-    def handleLogin():
-        username = usernameTb.get()
-        passwd = passwdTb.get()
-        loginAccount(username, passwd, conn, mycursor)
-        
-
-    submitButton = tk.Button(frame2, 
-                        text="Login",
-                        command=handleLogin)
-    submitButton.grid(row=2, column=0, columnspan=2)
 
 def main():
     menuText = "Select an option"
@@ -79,7 +15,7 @@ def main():
 
     button1 = tk.Button(frame1, 
                         text="Login",
-                        command=prompt,
+                        command=lambda: login(window, frame1),
                         activebackground="blue", 
                         activeforeground="white",
                         anchor="center",
@@ -102,6 +38,7 @@ def main():
     button1.pack()
     button2 = tk.Button(frame1, 
                         text="Signup", 
+                        command=lambda: create(window, frame1),
                         activebackground="blue", 
                         activeforeground="white",
                         anchor="center",
@@ -124,6 +61,7 @@ def main():
     button2.pack()
     button3 = tk.Button(frame1, 
                         text="Delete", 
+                        command=lambda: delete(window, frame1),
                         activebackground="blue", 
                         activeforeground="white",
                         anchor="center",
@@ -172,24 +110,3 @@ frame1.pack()
 main()
 
 window.mainloop()
-
-    # prompt = input("What do you want to do? ")
-
-    # if prompt == "1":
-    #     username = input("Username: ")
-    #     passwd = input("Password: ").encode('utf-8')
-    #     loginAccount(username, passwd, conn, mycursor)
-    # elif prompt == "2":
-    #     username = input("Username: ")
-    #     passwd = input("Password: ").encode('utf-8')
-    #     createAccount(username, passwd, conn, mycursor)
-    # elif prompt == "3":
-    #     username = input("Username: ")
-    #     passwd = input("Password: ").encode('utf-8')
-    #     deleteAccount(username, passwd, conn, mycursor)
-    # elif prompt == "4":
-    #     print("Goodbye!")
-    #     quit()
-    # else:
-    #     print("\nInvalid option. Pick again!\n")
-    #     main()
---
+++ utils/mainMenu.py
@@ -0,0 +1,124 @@
+from utils.account import loginAccount, createAccount, deleteAccount
+import cred
+import mysql.connector
+from mysql.connector import Error
+from tkinter import *
+import tkinter as tk
+
+try:
+    conn = mysql.connector.connect(
+        host=cred.hostname,
+        user=cred.username,
+        passwd=cred.passwd,
+    )
+
+    if conn.is_connected():
+        mycursor = conn.cursor()
+        # Create database if not exist
+        mycursor.execute(f"CREATE DATABASE IF NOT EXISTS {cred.database};")
+        # print(f"Database '{cred.database}' created or already existed.")
+
+        # Use the newly created database
+        mycursor.execute(f"USE {cred.database};")
+        # print(f"Using database '{cred.database}'.")
+
+except Error as e:
+    print(f"Error: {e}")
+    quit()
+
+mycursor.execute("""CREATE TABLE IF NOT EXISTS Users
+                    (userId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                     username VARCHAR(50) NOT NULL,
+                     passwdHash VARCHAR(255) NOT NULL,
+                     secretKey VARCHAR(255) NOT NULL);""")
+mycursor.execute("""CREATE TABLE IF NOT EXISTS Images
+                    (photoId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                     platform VARCHAR(50) NOT NULL,
+                     photo LONGBLOB NOT NULL,
+                     userId INT NOT NULL,
+                     FOREIGN KEY(userId) REFERENCES Users(userId));""")
+
+def prompt(frames):
+    usernameT = tk.Label(frames, text="Username")
+    usernameT.grid(row=0)
+
+    passwordT = tk.Label(frames, text="Password")
+    passwordT.grid(row=1)
+
+    usernameTb = Entry(frames)
+    usernameTb.grid(row=0, column=1)
+
+    passwdTb = Entry(frames, show='*')
+    passwdTb.grid(row=1, column=1)
+
+    return usernameTb, passwdTb
+
+def returnMain(curFrame, mainFrame):
+    curFrame.destroy()
+    mainFrame.pack()
+
+def login(window, frame1):
+    frame1.pack_forget()
+    frame2 = Frame(window)
+    frame2.pack()
+
+    usernameTb, passwdTb = prompt(frame2)
+
+    def handleLogin():
+        username = usernameTb.get()
+        passwd = passwdTb.get()
+        loginAccount(username, passwd, conn, mycursor)
+        
+    submitButton = tk.Button(frame2, 
+                        text="Login",
+                        command=handleLogin)
+    submitButton.grid(row=2, column=1)
+
+    returnButton = tk.Button(frame2,
+                             text="Cancel",
+                             command=lambda: returnMain(frame2, frame1))
+    returnButton.grid(row=2, column=0)
+
+def create(window, frame1):
+    frame1.pack_forget()
+    frame3 = Frame(window)
+    frame3.pack()
+
+    usernameTb, passwdTb = prompt(frame3)
+
+    def handleCreate():
+        username = usernameTb.get()
+        passwd = passwdTb.get()
+        loginAccount(username, passwd, conn, mycursor)
+        
+    submitButton = tk.Button(frame3, 
+                             text="Sign Up",
+                             command=handleCreate)
+    submitButton.grid(row=2, column=1)
+
+    returnButton = tk.Button(frame3,
+                             text="Cancel",
+                             command=lambda: returnMain(frame3, frame1))
+    returnButton.grid(row=2, column=0)
+
+def delete(window, frame1):
+    frame1.pack_forget()
+    frame4 = Frame(window)
+    frame4.pack()
+
+    usernameTb, passwdTb = prompt(frame4)
+
+    def handleDelete():
+        username = usernameTb.get()
+        passwd = passwdTb.get()
+        loginAccount(username, passwd, conn, mycursor)
+        
+    submitButton = tk.Button(frame4, 
+                             text="Delete",
+                             command=handleDelete)
+    submitButton.grid(row=2, column=1)
+
+    returnButton = tk.Button(frame4,
+                             text="Cancel",
+                             command=lambda: returnMain(frame4, frame1))
+    returnButton.grid(row=2, column=0)
